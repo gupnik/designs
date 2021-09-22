3e340bb52c858d7d8e46fca778177359759da976
Nikhil Gupta <nikhigu@adobe.com>
2021-09-22 11:08:31 +0530

    Adds ability to mint on devnet

--- jungleverse-react/src/components/btn-select-wallet/SelectWalletButton.js
+++ jungleverse-react/src/components/btn-select-wallet/SelectWalletButton.js
@@ -1,31 +1,89 @@
 import { Button, Modal } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import style from "./SelectWalletButton.module.css";
 import { useWalletState, WALLET_STATES } from "../../contexts/WalletContext";
 
+import * as anchor from "@project-serum/anchor";
+import { getCandyMachineState, mintOneToken } from "../../candy_machine/candy-machine";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import { useWalletDialog } from "@solana/wallet-adapter-material-ui";
+
+const config = new anchor.web3.PublicKey("B2vJUSDeLnSHFZrrB697X546aCDkDJLCuaJmm4Qx3RL8");
+
+const candyMachineId = new anchor.web3.PublicKey("2ujqBwvfyDstnKtnngad6BbsB8QnpYSmLz9pHJ2GENUp");
+
+const treasury = new anchor.web3.PublicKey("7fCPrREnGs86StF2md8gouxg8rELhKH15fsVnxyuADjP");
+
+const isMainnet = false;
+
+const rpcHost = isMainnet ? `https://api.mainnet-beta.solana.com/` : `https://api.devnet.solana.com/`;
+const connection = new anchor.web3.Connection(rpcHost);
+
 export default function SelectWalletButton({ children }) {
+  const wallet = useWallet();
+  const walletDialog = useWalletDialog();
+
+  const [candyMachine, setCandyMachine] = useState({});
+  const [itemsRemaining, setItemsRemaining] = useState(0);
+
   const [state, setState] = useWalletState();
   const [mintModalShow, setMintModalShow] = useState(false);
+  
 
-  const handleMintClick = () => setMintModalShow(true);
+  const handleMintClick = async () => {
+    setMintModalShow(true);
+    console.log("Mint called");
+    if (wallet.connected) {
+      console.log("Connected", wallet.connected, wallet.publicKey);
+      try {
+        let result = await mintOneToken(candyMachine, config, wallet.publicKey, treasury);
+        console.log(result);
+      } catch (error) {
+        alert("You don't have enough SOL in your wallet to mint");
+      } finally {
+        setMintModalShow(false);
+      }
+    }
+  }
   const handleMintModalHide = () => setMintModalShow(false);
 
   function handleSelectWalletClick() {
+    walletDialog.setOpen(true);
     setState(WALLET_STATES.MINT);
   }
 
+  useEffect(() => {
+    (async () => {
+      const anchorWallet = {
+        publicKey: wallet.publicKey,
+        signAllTransactions: wallet.signAllTransactions,
+        signTransaction: wallet.signTransaction,
+      };
+
+      const { candyMachine, goLiveDate, itemsRemaining } =
+        await getCandyMachineState(
+          anchorWallet,
+          candyMachineId,
+          connection
+        );
+      setItemsRemaining(itemsRemaining);
+      setCandyMachine(candyMachine);
+    })();
+  }, [wallet, setCandyMachine, setItemsRemaining]);
+
   return (
     <>
-      {state === WALLET_STATES.SELECT_WALLET ? (
-        <Button
-          style={{ border: "3px solid black" }}
-          className="px-4 py-2"
-          variant="primary"
-          onClick={handleSelectWalletClick}
-        >
-          Select Wallet
-        </Button>
+      {!wallet.connected ? (
+         <Button
+         style={{ border: "3px solid black" }}
+         className="px-4 py-2"
+         variant="primary"
+         onClick={handleSelectWalletClick}
+       >
+         Select Wallet
+       </Button>
       ) : (
         <>
           <Modal
--- jungleverse-react/src/routes.js
+++ jungleverse-react/src/routes.js
@@ -1,18 +1,45 @@
+import { useState, useEffect, useMemo } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import IndexPage from "./pages";
 import IndexPageV2 from "./pages/v2";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+} from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+
+const network = "devnet";
 
 export default function Routes() {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [getPhantomWallet(), getSolflareWallet(), getSolletWallet()],
+    []
+  );
+
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/v1">
-          <IndexPage></IndexPage>
-        </Route>
-        <Route exact path="/">
-          <IndexPageV2></IndexPageV2>
-        </Route>
-      </Switch>
-    </HashRouter>
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletDialogProvider>
+            <HashRouter>
+              <Switch>
+                <Route path="/v1">
+                  <IndexPage></IndexPage>
+                </Route>
+                <Route exact path="/">
+                  <IndexPageV2></IndexPageV2>
+                </Route>
+              </Switch>
+            </HashRouter>
+          </WalletDialogProvider>
+        </WalletProvider>
+    </ConnectionProvider>
   );
 }
